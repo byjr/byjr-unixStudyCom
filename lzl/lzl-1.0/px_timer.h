0047e35f05868990a4c448e0579a45220b74ea89
Your Name <you@example.com>
2018-02-08 13:40:26 +0800

    add un_fifo

--- lzl/lzl-1.0/px_timer.h
+++ lzl/lzl-1.0/px_timer.h
@@ -1,9 +1,7 @@
 #ifndef _UN_RWLOCK_H
 #define _UN_RWLOCK_H 1
-// #define __USE_GNU 1
-/*//  ____________________________________________________________________________
-typedef long clock_t;
-typedef long time_t;
+#define __USE_GNU 1
+#if 0
 
 extern clock_t clock(void);//进程启动到当前调用处的节拍数 failure return -1;
 extern time_t time(time_t *__timer);//unix纪元当前调用处的时间(秒) failure return -1;
@@ -31,9 +29,8 @@ extern int clock_gettime(clockid_t __clock_id,struct timespec *__tp);
 extern int clock_settime(clockid_t __clock_id,const struct timespec *__tp);
 extern int clock_nanosleep(clockid_t __clock_id,int __flags,const struct timespec *__req,struct timespec *__rem);//flag:0,相对(调用处)时间；1，绝对时间
 extern int clock_getcpuclockid(pid_t __pid,clockid_t *__clock_id);
-extern int pthread_getcpuclockid(pthread_t __thread_id,
-				  __clockid_t *__clock_id);
 extern int timer_create(clockid_t __clock_id,struct sigevent * __evp,timer_t * __timerid);
+
 extern int timer_delete(timer_t __timerid);
 extern int timer_settime(timer_t __timerid,int __flags,const struct itimerspec * __value,struct itimerspec *__ovalue);
 extern int timer_gettime(timer_t __timerid,struct itimerspec *__value);//获取剩余时间（减计数器）
@@ -45,67 +42,54 @@ extern int getdate_r(const char * __string,struct tm * __resbufp);
 extern int timerfd_gettime (int __ufd, struct itimerspec *__otmr);
 extern int timerfd_settime (int __ufd, int __flags,const struct itimerspec *__utmr,struct itimerspec *__otmr);
 extern int timerfd_create (clockid_t __clock_id, int __flags);
-							/* 				linux 时钟类型
-								CLOCK_REALTIME 				:墙上时钟
-								CLOCK_MONOTONIC 			:单调时钟
-								CLOCK_MONOTONIC_RAW 		:开机单调时钟
-								CLOCK_PROCESS_CPUTIME_ID 	:进程时钟
-								CLOCK_THREAD_CPUTIME_ID		:线程时钟 		*/
-// ____________________________________________________________________________ */
 
-#define SIG_CS_CMD SIGRTMIN
+extern int settimeofday(const struct timeval *__tv, const struct timezone *__tz);
+extern int adjtime(const struct timeval *__delta,struct timeval *__olddelta);
+extern int getitimer(__itimer_which_t __which,struct itimerval *__value);
+extern int setitimer(__itimer_which_t __which,const struct itimerval * __new,struct itimerval * __old);
+extern int utimes(const char *__file, const struct timeval __tvp[2]);
+extern int lutimes(const char *__file, const struc t timeval __tvp[2]);
+extern int futimes(int __fd, const struct timeval __tvp[2]);
+extern int futimesat(int __fd, const char *__file,const struct timeval __tvp[2]);
+#endif
+
+long get_tsd(struct timespec *pt1,struct timespec *pt2,char lev);
+
+#define get_tsv(ptv,pt2,pt1) ({\
+	ptv->tv_sec=pt2->tv_sec-pt1->tv_sec;\
+	ptv->tv_nsec=pt2->tv_nsec-pt1->tv_nsec;\
+	ptv;\
+})
+
 #define px_timer_create(cid,p_evp,id)({\
 	int ret=0;\
 	ret=timer_create(cid,p_evp,id);\
-	if(-1==ret){\
-		show_errno(ret,__func__);\
-		exit(-1);\
-	}\
+	if(-1==ret)	show_errno(ret,"px_timer_create");\
+	ret?-1:0;\
 })
 #define px_timer_set(id,flag,p_v,p_ov)({\
 	int ret=0;\
 	ret=timer_settime(id,flag,p_v,p_ov);\
-	if(-1==ret){\
-		show_errno(ret,__func__);\
-		exit(-1);\
-	}\
+	if(-1==ret)	show_errno(ret,"px_timer_set");\
+	ret?-1:0;\
 })
 #define px_timer_get(id,p_v)({\
 	int ret=0;\
 	ret=timer_gettime(id,p_v);\
-	ret=ret?-1:0;\
+	if(-1==ret)	show_errno(ret,"px_timer_get");\
+	ret?-1:0;\
 })
 #define px_timer_del(id)({\
 	int ret=0;\
 	ret=timer_delete(id);\
-	if(-1==ret){\
-		show_errno(ret,__func__);\
-		exit(-1);\
-	}\
+	if(-1==ret)	show_errno(ret,"px_timer_del");\
+	ret?-1:0;\
 })
 #define px_timer_getoverrun(id)({\
 	int ret=0;\
 	ret=timer_getoverrun(id);\
-	switch(ret){\
-	case DELAYTIMER_MAX:\
-		war("overrun:%d",DELAYTIMER_MAX);\
-		break;\
-	case -1:\
-		show_errno(ret,__func__);\
-		break;\
-	default:break;\
-	}\
-})
-#define px_thread_getcpuclockid(tid,p_cid)({\
-	int ret=0;\
-	ret=pthread_getcpuclockid(tid,p_cid);\
-	if(ret)show_errno(ret,__func__);\
+	if(-1==ret)	show_errno(ret,"px_timer_getoverrun");\
 	ret?-1:0;\
 })
-#define process_getcpuclockid(pid,p_cid)({\
-	int ret=0;\
-	ret=clock_getcpuclockid(pid,p_cid);\
-	if(ret)show_errno(ret,__func__);\
-	ret?-1:0;\
-})
+#define SIG_CS_CMD SIGRTMIN
 #endif
